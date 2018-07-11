f890e85def9b630e75051d06d33d20395b2afd0a
mmatthews06 <mmatthews06@gmail.com>
2018-07-10 21:10:41 -0400

    fixed margins for X SVG, but requires passing height and width

--- src/components/game-pieces/x-svg/XPieceSVG.js
+++ src/components/game-pieces/x-svg/XPieceSVG.js
@@ -7,14 +7,18 @@ import EmptyPieceSVG from '../empty-svg/EmptyPieceSVG';
 import './XPieceSVG.css';
 
 export default function XPieceSVG({ margin }) {
-  const endMargin = 100 - margin;
+  // const endMargin = 100 - margin;
+  const width = 167 - margin;
+  const height = 167 - margin;
 
   return (
     // TODO: review whether this svg element is needed.
-    <svg x="0" y="0" height="100%" width="100%" className="x-piece-svg">
+    <svg x="0" y="0" height="167" width="167" className="x-piece-svg">
       <EmptyPieceSVG />
-      <line x1={`${margin}%`} y1={`${margin}%`} x2={`${endMargin}%`} y2={`${endMargin}%`} />
-      <line x1={`${endMargin}%`} y1={`${margin}%`} x2={`${margin}%`} y2={`${endMargin}%`} />
+      <g transform={`translate(${margin}, ${margin});`}>
+        <line x1={margin} y1={margin} x2={width} y2={height} />
+        <line x1={width} y1={margin} x2={margin} y2={height} />
+      </g>
     </svg>
   );
 }
