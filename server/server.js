f15354e01395bd3a6b73d08c35fba7f8974213c2
mmatthews06 <mmatthews06@gmail.com>
2018-07-15 10:58:50 -0400

    TicTacToe - make eslint work again after ejection, by removing duplicate package.json entry

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../build')));
 app.use(express.json());
 
+// Add APIs to the app.
 api(app);
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
