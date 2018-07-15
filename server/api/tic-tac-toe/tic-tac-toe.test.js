d237495b6e490a799eabd4a7c316a733bb910537
mmatthews06 <mmatthews06@gmail.com>
2018-07-15 13:33:24 -0400

    TTT API - computer randomly takes the first move.

--- server/api/tic-tac-toe/tic-tac-toe.test.js
+++ server/api/tic-tac-toe/tic-tac-toe.test.js
@@ -3,16 +3,31 @@ const request = require('supertest');
 const app = require('./tic-tac-toe');
 
 describe('Tic-Tac-Toe Express App', () => {
-  it('should return a new board.', async () => {
+  it('should return 200 for a new board.', async () => {
     const response = await request(app).get('/new');
     expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual({
-      board: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-      turn: 0,
+  });
+
+  it('should return a valid board with potentially 1 move.', async () => {
+    expect.assertions(3);
+
+    const response = await request(app).get('/new');
+    expect(response.statusCode).toBe(200);
+
+    const game = response.body;
+    const sum = game.board.reduce((s, i) => s + i, 0);
+
+    expect(game).toMatchObject({
       ended: false,
       winner: null,
       player1: 1,
       player2: 4,
     });
+
+    if (game.turn === 1) {
+      expect(sum).toBe(4);
+    } else {
+      expect(sum).toBe(1);
+    }
   });
 });
