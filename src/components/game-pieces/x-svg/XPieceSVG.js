e4a9c1bed7c0909b436e5b56cf8ed60f6464d6f0
mmatthews06 <mmatthews06@gmail.com>
2018-07-16 01:12:48 -0400

    UI - significantly better looks

--- src/components/game-pieces/x-svg/XPieceSVG.js
+++ src/components/game-pieces/x-svg/XPieceSVG.js
@@ -10,8 +10,7 @@ export default function XPieceSVG({ margin }) {
   const endMargin = 100 - margin;
 
   return (
-    // TODO: review whether this svg element is needed.
-    <svg x="0" y="0" height="100%" width="100%" className="x-piece-svg">
+    <svg x="0" y="0" height="100%" width="100%" viewBox="0 0 100 100" className="x-piece-svg">
       <EmptyPieceSVG />
       <line x1={`${margin}%`} y1={`${margin}%`} x2={`${endMargin}%`} y2={`${endMargin}%`} />
       <line x1={`${endMargin}%`} y1={`${margin}%`} x2={`${margin}%`} y2={`${endMargin}%`} />
--- src/containers/game-board-container/GameBoardContainer.test.js
+++ src/containers/game-board-container/GameBoardContainer.test.js
@@ -3,8 +3,8 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import GameBoardContainer from './GameBoardContainer';
 
-function givenBoard(board) {
-  const promise = Promise.resolve({ json: () => ({ board }) });
+function givenBoard(board, ended = false, winner = null, player1 = 1) {
+  const promise = Promise.resolve({ json: () => ({ board, ended, winner, player1 }) });
   global.fetch = jest.fn(() => promise);
   return promise;
 }
@@ -20,6 +20,20 @@ describe('GameBoardContainer', () => {
     expect(global.fetch).toHaveBeenCalledWith('/tic-tac-toe/new');
   });
 
+  it('should start with a loading state', async (done) => {
+    expect.assertions(2);
+    const board = [0, 1, 4, 0, 1, 4, 0, 1, 4];
+    const promise = givenBoard(board);
+    const wrapper = mount(<GameBoardContainer />);
+    expect(wrapper.state('isLoading')).toBe(true);
+    await promise;
+    return promise.then(() => {
+      wrapper.update();
+      expect(wrapper.state('isLoading')).toBe(false);
+      done();
+    }).catch(() => done.fail());
+  });
+
   it('should set state', async (done) => {
     expect.assertions(2);
     const board = [0, 1, 4, 0, 1, 4, 0, 1, 4];
@@ -31,6 +45,55 @@ describe('GameBoardContainer', () => {
       wrapper.update();
       expect(wrapper.state('game').board).toEqual(board);
       done();
+    }).catch(() => done.fail());
+  });
+
+  describe('New Game Button', () => {
+    it('should call for a new game', () => {
+      const board = [0, 1, 4, 0, 1, 4, 0, 1, 4];
+      givenBoard(board);
+      const wrapper = mount(<GameBoardContainer />);
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+      wrapper.find('Button').simulate('click');
+      expect(global.fetch).toHaveBeenCalledTimes(2);
+    });
+  });
+
+  describe('End Game Messaging', () => {
+    it('should show a draw', async (done) => {
+      const promise = givenBoard([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], true, null);
+      const wrapper = mount(<GameBoardContainer />);
+      await promise;
+      return promise.then(() => {
+        wrapper.update();
+        const message = wrapper.instance().gameEndMessage();
+        expect(message).toBe('Draw. Try again.');
+        done();
+      }).catch(() => done.fail());
+    });
+
+    it('should say when player1 wins', async (done) => {
+      const promise = givenBoard([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], true, 1);
+      const wrapper = mount(<GameBoardContainer />);
+      await promise;
+      return promise.then(() => {
+        wrapper.update();
+        const message = wrapper.instance().gameEndMessage();
+        expect(message).toBe('Player 1 won!');
+        done();
+      }).catch(() => done.fail());
+    });
+
+    it('should say when player2 wins', async (done) => {
+      const promise = givenBoard([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], true, 4);
+      const wrapper = mount(<GameBoardContainer />);
+      await promise;
+      return promise.then(() => {
+        wrapper.update();
+        const message = wrapper.instance().gameEndMessage();
+        expect(message).toBe('Player 2 won!');
+        done();
+      }).catch(() => done.fail());
     });
   });
 });
